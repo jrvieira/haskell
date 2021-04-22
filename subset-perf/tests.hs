ef4b4843c41b7d51a5cf54648b9e8db87ca7d6fa
jrvieira <github@jrvieira.com>
2021-04-22 06:38:57 +0100

    test results updated

--- subset-perf/tests.hs
+++ subset-perf/tests.hs
@@ -1,14 +1,15 @@
 -- SUBSETS performance test
 -- ghci: length $ sX [0..19] -- eee
--- bench: length $ sX [0..26] -- ubuntu vm imac
+-- ghc -O0,O1,O2: length $ sX [0..26] -- ubuntu vm imac (bench)
 
 -- naive
 s0 = s
    where
    s [] = [[]]
    s (x:xs) = map (x :) (s xs) ++ (s xs)
-
--- ~ 15 s
+-- ~ 15 s <- ghci probably computes (s xs) twice
+-- ~ 56 s
+-- ~ 14 s <- -O1 optimize with memoization
 -- ~ 13 s
 
 -- memoized
@@ -18,7 +19,9 @@ s1 = s
    s (x:xs) = map (x :) ss ++ ss
       where
       ss = s xs
--- ~ 1.5 s
+-- ~ 1.5 s <- memoization is awesome
+-- ~ 15 s
+-- ~ 13 a
 -- ~ 13 s
 
 -- iterative
@@ -29,6 +32,8 @@ s2 = s
       where
       ss _ [] = []
       ss x (y:ys) = (x:y) : y : ss x ys
--- ~ 3.2 s
+-- ~ 3 s <- no idea why this is slower than s1
+-- ~ 1.8 s
 -- ~ 1.5 s
+-- ~ 1.5 s <- by far the fastest solution with compiler optimizations
 
