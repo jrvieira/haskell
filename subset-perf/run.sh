4574e5862f6c81ff49b724f5287a8c81030a46b7
jrvieira <github@jrvieira.com>
2021-04-23 13:31:24 +0100

    update run.hs

--- subset-perf/run.sh
+++ subset-perf/run.sh
@@ -1,16 +1,21 @@
+echo "Compiling O0..."
 mkdir O0
+ghc bench -O0 -fforce-recomp -j -no-keep-hi-files -no-keep-o-files
+mv bench O0
+echo "Compiling O1..."
 mkdir O1
+ghc bench -O1 -fforce-recomp -j -no-keep-hi-files -no-keep-o-files
+mv bench O1
+echo "Compiling O2..."
 mkdir O2
-echo "Compiling..."
-ghc bench -O0 -outputdir O0 -fforce-recomp -j -no-keep-hi-files -no-keep-o-files
-ghc bench -O1 -outputdir O1 -fforce-recomp -j -no-keep-hi-files -no-keep-o-files
-ghc bench -O2 -outputdir O2 -fforce-recomp -j -no-keep-hi-files -no-keep-o-files
+ghc bench -O2 -fforce-recomp -j -no-keep-hi-files -no-keep-o-files
+mv bench O2
 echo "Benchmarking..."
 touch bench.txt
-echo "BENCHMARK\n\n" > bench.txt
-echo "O0\n\n" >> bench.txt
+echo "BENCHMARK\n" > bench.txt
+echo "O0\n" >> bench.txt
 ./O0/bench --output bench-O0.html >> bench.txt
-echo "O1\n\n" >> bench.txt
+echo "O1\n" >> bench.txt
 ./O1/bench --output bench-O1.html >> bench.txt
-echo "O2\n\n" >> bench.txt
+echo "O2\n" >> bench.txt
 ./O2/bench --output bench-O2.html >> bench.txt
