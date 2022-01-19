1dae5d7ee45c8e7adb03989f63fee43672ba7903
jrvieira <github@jrvieira.com>
2022-01-19 17:08:47 +0000

    Y comment

--- mu.hs
+++ mu.hs
@@ -1,5 +1,12 @@
 main = print $ take 10 $ y (1:)
 
+{- Recursive type Mu guides the typechecking of the Y combinator
+   otherwise (\f -> (\x -> f (x x)) (\x -> f (x x))) fails with
+   error:
+    • Occurs check: cannot construct the infinite type: a0 ~ a0 -> b
+      Expected type: (a0 -> b) -> (a0 -> b) -> b
+-}
+
 newtype Mu a = Mu (Mu a -> a)
 
 y :: (a -> a) -> a
